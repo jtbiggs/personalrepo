c92f9d4897f097a8fb5787ae246e66142789d428
jtbiggs <jbiggs@email.com>
2024-06-07 12:19:31 -0500

    Lab2

--- R/STAT 509/Lab2/Lab2_Biggs/Lab2.r
+++ R/STAT 509/Lab2/Lab2_Biggs/Lab2.r
@@ -5,3 +5,5 @@ setwd("/home/justinbiggs/Documents/Code/personalrepo/R/STAT 509/Lab2")
 
 #import table data
 SEATS=read.csv("Lab2_Carseats.csv",header=T, na.strings="?")
+
+lm.fit=lm(Sales ~ CompPrice+Income+ShelveLoc, data=SEATS)
